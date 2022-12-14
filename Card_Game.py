7e856c595754432399e27125027a1c68824be6bd
Lama Abdulrahman <72937704+lamaAbdulrahmana@users.noreply.github.com>
2022-12-14 21:30:14 +0300

    Finally DONE!!!

--- Card_Game.py
+++ Card_Game.py
@@ -16,8 +16,11 @@ class Card(object):
         self.suit_score = suit_score
         
     def __str__(self):
-        print('{} {}'.format(self.value, self.suit,self.card_score,self.suit_score))
-        
+        return '{} {}'.format(self.value, self.suit)
+    
+    def __repr__(self) -> str:
+        print('{} {}'.format(self.value, self.suit))  
+     
     def __lt__(self, c2):
         if self.card_score < c2.card_score:
             return True
@@ -87,44 +90,44 @@ class Player(object):
  
 class Game(object):
     
-    def __init__(self,deck,players):
+    def __init__(self,deck):
         self.deck = deck
-        self.players = players
+    
+    def play_game(self):
+        print('Welcome to the card game ♠♥♦♣ created by lama')
+        players_numbers = int(input('Enter numbers of players: '))
+        players = []
+        for number in range(players_numbers):
+            name = str(input('Please enter player {} name: '.format(number+1)))
+            p = Player(name)
+            players.append(p)
+        choice = 1
+        for round in range(int(52/len(players))):
+            if (choice == 1):
+                print('''Choose an option to play or quit:
+                1. draw card
+                2. quit game''')
+                choice = int(input('Choose an option: '))
+                for player in players:
+                    player.add_card(self.deck)
+                    print('{} has pulled {}'.format(player.name,player.cards[0].__str__()))
+                played_cards = []
+                for player in players:
+                    played_cards.append(player.cards.pop())
+                winner_card = max(played_cards)
+                round_winner = players[played_cards.index(winner_card)]
+                round_winner.round_winner()
+                print('{} won round # {}'.format(round_winner.name,round+1))
+            else:
+                print('you choose to quit the game bye')
+                break
+        winner = max(players, key=attrgetter('wins'))
+        print('The winner is {} with {} wins HORAY'.format(winner.name, winner.wins)) 
         
-            
-
-def main():
-    players = []
-    print('Welcome to the card game ♠♥♦♣ created by lama')
-   
-
-d = Deck()
-p1 = Player('Lama')
-p2 = Player('Maha')
-p3 = Player('Dana')
-p4 = Player('Nouf')
-   
-
-players = [p1,p2,p3,p4]
-
-while len(d.cards)>0:
-    for player in players:
-        player.add_card(d) 
-
-    
-for round in range(int(52/len(players))):
-    played_cards = []
-    for player in players:
-        played_cards.append(player.cards.pop())
-    winner_card = max(played_cards)
-    round_winner = players[played_cards.index(winner_card)]
-    round_winner.round_winner()
-    print('{} won round # {}'.format(round_winner.name,round+1))
-
-    
-winner = max(players, key=attrgetter('wins'))
-print('The winner is {} with {} wins HORAY'.format(winner.name, winner.wins))    
-
+             
+deck = Deck()       
+game = Game(deck)
+game.play_game()              
 # d.shuffle()
 # d.__str__()
 # print(len(d.cards))
